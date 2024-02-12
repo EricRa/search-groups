f30aa0668c0d46986f3a37b4d2fbb448d6698344
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2024-02-12 16:40:38 -0500

    Add js for setting main search group labels

---
+++ main/main.js
@@ -0,0 +1,33 @@
+
+// Set labels for groups equal to the value attributes of their
+// corresponding input fields
+
+let c1 = document.getElementById("choice1").value
+document.getElementById("c1-label").textContent = c1
+
+let c2 = document.getElementById("choice2").value
+document.getElementById("c2-label").textContent = c2
+
+let c3 = document.getElementById("choice3").value
+document.getElementById("c3-label").textContent = c3
+
+let c4 = document.getElementById("choice4").value
+document.getElementById("c4-label").textContent = c4
+
+let c5 = document.getElementById("choice5").value
+document.getElementById("c5-label").textContent = c5
+
+let c6 = document.getElementById("choice6").value
+document.getElementById("c6-label").textContent = c6
+
+let c7 = document.getElementById("choice7").value
+document.getElementById("c7-label").textContent = c7
+
+let c8 = document.getElementById("choice8").value
+document.getElementById("c8-label").textContent = c8
+
+let c9 = document.getElementById("choice9").value
+document.getElementById("c9-label").textContent = c9
+
+let c10 = document.getElementById("choice10").value
+document.getElementById("c10-label").textContent = c10
